ab87a50459a40c2781d61d19d44072d52f83c444
Rajeevkumar K <krkumarid@gmail.com>
2022-11-10 01:53:53 -0500

    File added

---
+++ src/test/java/com/rest/api/get/GitTestClass.java
@@ -0,0 +1,12 @@
+package com.rest.api.get;
+
+import org.testng.annotations.Test;
+
+public class GitTestClass
+{
+    @Test
+    public static void testGit()
+    {
+        System.out.println("Testing the Git");
+    }
+}
